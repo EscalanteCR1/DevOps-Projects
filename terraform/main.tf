26512e21346ce47295e332651c0b64c4428ef281
EscalanteCR1 <emmanuel.escalantelobo@gmail.com>
2025-05-22 01:18:48 -0600

    Bucket Issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform{
     }
     backend "s3" {
         key            = "aws/ec2/terraform.tfstate"
-        bucket = "ec2-app-terraform-example-state-bucket"  
+        bucket = "ec2-example-app-terraform--state-bucket"  
     }
 }
 
