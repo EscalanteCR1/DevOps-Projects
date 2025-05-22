86fe9b20e5fc6f43fcccdc5e270d573db941ee90
EscalanteCR1 <emmanuel.escalantelobo@gmail.com>
2025-05-22 03:08:31 -0600

    Terraform script issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_instance" "server" {
     
-    ami           = "ami-03250b0e01c28d196"
+    ami           = "ami-084568db4383264d4"
     instance_type = "t2.micro"
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [ aws_security_group.terraformsecgroup.id ]
