20ee9506fee0ff9ba5820b33fa7baa8171219dd6
EscalanteCR1 <emmanuel.escalantelobo@gmail.com>
2025-05-22 02:11:57 -0600

    Terraform script issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
     region = var.region
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "server" {
     
     ami           = "ami-03250b0e01c28d196"
     instance_type = "t2.micro"
